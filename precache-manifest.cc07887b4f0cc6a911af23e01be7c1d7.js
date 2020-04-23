49183a04b525d765d167e139e40aecb3e0b84053
Zomdies <sablezubi@mail.ru>
2020-04-23 22:23:34 +0700

    First Commit

---
+++ precache-manifest.cc07887b4f0cc6a911af23e01be7c1d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d5b05fb7da299c312bbf13a0f92882",
+    "url": "/TaskToday/index.html"
+  },
+  {
+    "revision": "9268f1b2441cb5bb4c68",
+    "url": "/TaskToday/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d9d49a65be2f48c84764",
+    "url": "/TaskToday/static/css/main.c9483c98.chunk.css"
+  },
+  {
+    "revision": "9268f1b2441cb5bb4c68",
+    "url": "/TaskToday/static/js/2.37300507.chunk.js"
+  },
+  {
+    "revision": "8c323eee9c0820e0e208cd7dbdc413ce",
+    "url": "/TaskToday/static/js/2.37300507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d49a65be2f48c84764",
+    "url": "/TaskToday/static/js/main.9d493808.chunk.js"
+  },
+  {
+    "revision": "a244d4ba7228bd1a3c6f",
+    "url": "/TaskToday/static/js/runtime-main.a7f1b498.js"
+  }
+]);
