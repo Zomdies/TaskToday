f468e6b2a52eb4d9387b21a9d2d31a5c568b960f
Zomdies <sablezubi@mail.ru>
2020-04-28 20:47:50 +0700

    Remove

---
+++ precache-manifest.c0577074e964e1b1531b9bd3daa5d963.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecebb7ceec612664d756726bf207897",
+    "url": "/TaskToday/index.html"
+  },
+  {
+    "revision": "d5c2ed84be50d68e950a",
+    "url": "/TaskToday/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ee871e128307aa786d1a",
+    "url": "/TaskToday/static/css/main.d85f0c8d.chunk.css"
+  },
+  {
+    "revision": "d5c2ed84be50d68e950a",
+    "url": "/TaskToday/static/js/2.30534b2a.chunk.js"
+  },
+  {
+    "revision": "8c323eee9c0820e0e208cd7dbdc413ce",
+    "url": "/TaskToday/static/js/2.30534b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee871e128307aa786d1a",
+    "url": "/TaskToday/static/js/main.60160e3b.chunk.js"
+  },
+  {
+    "revision": "a244d4ba7228bd1a3c6f",
+    "url": "/TaskToday/static/js/runtime-main.a7f1b498.js"
+  },
+  {
+    "revision": "5c5918c7f8abf8875dcc90035eda2b20",
+    "url": "/TaskToday/static/media/iconEmployeeProfile.5c5918c7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TaskToday/precache-manifest.3c931f28ecb5e024fec72ff8cd03a6ef.js"
+  "/TaskToday/precache-manifest.c0577074e964e1b1531b9bd3daa5d963.js"
 );
 
 self.addEventListener('message', (event) => {
